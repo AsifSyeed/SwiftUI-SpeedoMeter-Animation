bedec85a711d9dfc634dfebf5a044373d066050a
Asif Syeed <asifsyeed666@gmail.com>
2023-08-31 12:26:11 +0600

    Initial Commit

---
+++ SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by Asif Syeed on 31/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
